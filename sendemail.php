935290078b3534b6652334c095483f8b7e64c6d1
MD FIROJ UDDIN <48468312+FirojUddinA@users.noreply.github.com>
2019-05-24 08:59:44 +0600

    logo change

--- sendemail.php
+++ sendemail.php
@@ -26,7 +26,7 @@ if(isset($_POST['first_name'])&& isset($_POST['email'])){
     $send = mail($to,$subject,$body,$headers);
 }
 
-header('Location:http://serramart.com.mx/contact.html');
+header('Location:https://firojuddina.github.io/de/contact.html');
 exit;
 
 ?>
