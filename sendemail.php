5baee0fd30b417d47743bd014832dc3300bfee1e
MD FIROJ UDDIN <48468312+FirojUddinA@users.noreply.github.com>
2019-05-24 08:50:22 +0600

    email

--- sendemail.php
+++ sendemail.php
@@ -1,19 +1,21 @@
 <?php 
 
-if(isset($_POST['name'])&& isset($_POST['email'])){
-    $name = $_POST['name'];
+if(isset($_POST['first_name'])&& isset($_POST['email'])){
+    $first_name = $_POST['first_name'];
+    $last_name = $_POST['last_name'];
     $email = $_POST['email'];
-    $phone = $_POST['phone'];
+    $telephone = $_POST['telephone'];
     $subject = "Contact request";
-    $message = $_POST['message'];
+    $message = $_POST['comments'];
     $to = 'firoj.uddin2580@gmail.com';
     $body = ' <html>
                 <body>
                     <h2>Message From The Outcome Network</h2>
                     <hr>
-                    <p>Name :'.$name.'</p>
+                    <p>First Name :'.$first_name.'</p>
+                    <p>Last Name :'.$last_name.'</p>
                     <p> Email :'.$email.'</p>
-                    <p> Phone :'.$phone.'</p>
+                    <p> Phone :'.$telephone.'</p>
                     <h3>Message</h3>
                     <p>'.$message.'</p>
                 </body>
